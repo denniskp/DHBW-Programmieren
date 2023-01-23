0fbea1f00515c8e018ce2d5986711883f94de66f
Dennis <122602355+denniskp@users.noreply.github.com>
2023-01-23 11:18:40 +0100

    fix: refactor code for newbies

--- src/main/java/de/dhbwka/java/exercise/control/NumberGuess.java
+++ src/main/java/de/dhbwka/java/exercise/control/NumberGuess.java
@@ -2,58 +2,34 @@ package de.dhbwka.java.exercise.control;
 
 import java.util.Scanner;
 
-class NumberGuessGame {
-    private final Scanner scanner = new Scanner(System.in);
-    private final String username;
-
-    public NumberGuessGame() {
-        username = askUsername();
-    }
-
-    public String askUsername() {
-        System.out.print("Wie ist dein Name? ");
-        return scanner.next();
-    }
-
-    public int generateRandomNumber() {
-        return (int)(Math.random() * 100);
-    }
-
-    public boolean wantsToPlayAgain() {
-        System.out.println("Was möchtest du tun?");
-        System.out.println("0 - Das Spiel beenden");
-        System.out.print("1 - Das Spiel fortsetzen ");
-        return scanner.nextInt() == 1;
-    }
-
-    public void run() {
-        int randomNumber = generateRandomNumber();
-
-        int tries = 1;
-        while (true) {
-            System.out.print(username + ", rate eine Zahl [1-100]: ");
-            int input = scanner.nextInt();
-
-            if (input < randomNumber) {
-                System.out.printf("Versuch %d: %d ist zu niedrig.\n", tries, input);
-            } else if (input > randomNumber) {
-                System.out.printf("Versuch %d: %d ist zu hoch.\n", tries, input);
-            } else {
-                System.out.printf("Versuch %d: %d ist korrekt.\n", tries, input);
-                break;
-            }
-
-            ++tries;
-        }
-
-        if (wantsToPlayAgain())
-            run();
-    }
-}
-
 public class NumberGuess {
     public static void main(String[] args) {
-        NumberGuessGame game = new NumberGuessGame();
-        game.run();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Wie ist dein Name? ");
+        String username = scanner.next();
+
+        do {
+            int randomNumber = (int) (Math.random() * 100);
+
+            int tries = 1;
+            while (true) {
+                System.out.printf("%s, rate eine Zahl [1-100]: ", username);
+                final int input = scanner.nextInt();
+
+                if (input < randomNumber) {
+                    System.out.printf("Versuch %d: %d ist zu niedrig.\n", tries++, input);
+                } else if (input > randomNumber) {
+                    System.out.printf("Versuch %d: %d ist zu hoch.\n", tries++, input);
+                } else {
+                    System.out.printf("Versuch %d: %d ist korrekt.\n", tries, input);
+                    break;
+                }
+            }
+
+            System.out.println("Was möchtest du tun?");
+            System.out.println("0 - Das Spiel beenden");
+            System.out.print("1 - Das Spiel fortsetzen ");
+        } while (scanner.nextInt() != 0);
     }
 }

b84060a804405ea5c346dd3819bb91fb0311bf50
Dennis <122602355+denniskp@users.noreply.github.com>
2023-02-13 10:13:47 +0100

    fix: code optimization

--- src/main/java/de/dhbwka/java/exercise/arrays/MatrixSubtraction.java
+++ src/main/java/de/dhbwka/java/exercise/arrays/MatrixSubtraction.java
@@ -27,7 +27,7 @@ public class MatrixSubtraction {
     public static int[][] subMatrix(int[][] matrix1, int[][] matrix2) {
         int rows = matrix1.length;
         int columns = matrix1[0].length;
-        int[][] res = new int[matrix1.length][matrix1[0].length];
+        int[][] res = new int[rows][columns];
         for (int i = 0; i < rows; ++i) {
             for (int j = 0; j < columns; ++j) {
                 res[i][j] = matrix1[i][j] - matrix2[i][j];

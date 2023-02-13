31f2687160c78d6da555a16b829b8ad2f7a1fee7
Dennis <122602355+denniskp@users.noreply.github.com>
2023-02-13 10:59:46 +0100

    fix: test project

--- src/main/java/de/dhbwka/java/exercise/packages/HelloWorld.java
+++ src/main/java/de/dhbwka/java/exercise/packages/HelloWorld.java
@@ -2,6 +2,6 @@ package de.dhbwka.java.exercise.packages;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello World!");
     }
 }
